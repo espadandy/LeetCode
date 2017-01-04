58fe80c636f32a7d4b2a5cb9edd74b9367354424
espadandy <espadandy@gmail.com>
2017-01-04 15:03:48 -0500

    Finish the 258th

---
+++ 258.Add Digits/src/andy.java
@@ -0,0 +1,13 @@
+
+public class andy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int input;
+		for(input = 1;input<101;input++){
+			Solution s = new Solution();
+			System.out.println(input + " " + s.addDigits(input));
+		}
+	}
+
+}
